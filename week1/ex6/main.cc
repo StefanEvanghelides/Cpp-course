9bda0266806c0c1e59bb326084abbc85fb75fcaf
Remi <r.f.niel@student.rug.nl>
2018-09-13 23:31:09 +0200

    removed 'magic number' in ex6

--- week1/ex6/main.cc
+++ week1/ex6/main.cc
@@ -5,12 +5,12 @@ using namespace std;
 
 void output_odd_even(size_t number)
 {
-    size_t value = std::numeric_limits<size_t>::max() - 1;
+    size_t value = numeric_limits<size_t>::max() - 1;
 
     cout << (number != ((number / 2) * 2) ? "odd" : "even") << "\n";
     cout << (number & 1 ? "odd" : "even") << "\n";
     cout << (number % 2 ? "odd" : "even") << "\n";
-    cout << (number << (sizeof(size_t) * 8 - 1) ? "odd" : "even") << "\n";
+    cout << (number << (numeric_limits<size_t>::digits - 1) ? "odd" : "even") << "\n";
     cout << (((number | value) ^ value) ? "odd" : "even") << "\n";
 }
 
