a4c138af51ccc4a31d36f034743dc6c59ff7f312
Jos van Goor <josvangoor@gmail.com>
2018-09-14 19:03:28 +0200

    Fixxes week1

--- week1/ex6/main.cc
+++ week1/ex6/main.cc
@@ -1,24 +1,16 @@
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
-void output_odd_even(size_t number)
+int main()
 {
-    size_t value = numeric_limits<size_t>::max() - 1;
+    size_t number;
+    cin >> number; //read number from cin
 
+    //5 solutions
     cout << (number != ((number / 2) * 2) ? "odd" : "even") << "\n";
     cout << (number & 1 ? "odd" : "even") << "\n";
     cout << (number % 2 ? "odd" : "even") << "\n";
-    cout << (number << (numeric_limits<size_t>::digits - 1) ? "odd" : "even") << "\n";
-    cout << (((number | value) ^ value) ? "odd" : "even") << "\n";
-}
-
-int main()
-{
-    cout << "inputting 25\n";
-    output_odd_even(25);
-
-    cout << "\ninputting 12\n";
-    output_odd_even(12);
+    cout << (number << (sizeof(size_t) * 8 - 1) ? "odd" : "even") << "\n";
+    cout << (((number | ~1) ^ ~1) ? "odd" : "even") << "\n";
 }
--- week1/ex7/main.cc
+++ week1/ex7/main.cc
@@ -1,2 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
+int main()
+{
+    size_t input;
+    cin >> input;           //read input from cin
+
+    if (input & (input - 1)) // any pow2-1 is all ones, so we mask and check if it is 0
+        cout << "the value x is not an exact power of two\n";
+    else
+        cout << "the value x is an exact power of two\n";
+}
