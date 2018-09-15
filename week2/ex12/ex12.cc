a827db2832cb150ec2ee2dc19ccaf7a2c644bb82
Remi <r.f.niel@student.rug.nl>
2018-09-15 12:15:55 +0200

    Finished ex12

---
+++ week2/ex12/ex12.cc
@@ -0,0 +1,19 @@
+#include <iostream>;
+#include <cctype>;
+#include <string>;
+
+using namespace std;
+
+int main()
+{
+	string alphaCharSet;
+	
+	for (size_t idx = 0; idx != 256; ++idx)
+	{
+		char ch = char(idx);
+		if (isalpha(ch))		//Determine if the char is a letter
+			alphaCharSet += ch;	//If so at it to the char set
+	}
+
+	cout << alphaCharSet << '\n';
+}
