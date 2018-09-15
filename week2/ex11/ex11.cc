d5ce9d8d0b7ee9c4af31ec69ee007c377e66c6c4
Jos van Goor <josvangoor@gmail.com>
2018-09-15 15:55:34 +0200

    removed useless file and added 2 spaces to ex11

--- week2/ex11/ex11.cc
+++ week2/ex11/ex11.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	size_t number=stoul(argv[1]); //Read the input
+	size_t number = stoul(argv[1]); //Read the input
 
 	for (size_t idx = 1; idx != 11; ++idx)
 	{
