957cf01966896e8e13b16d3d64a056c94455992c
Remi <r.f.niel@student.rug.nl>
2018-09-15 11:54:48 +0200

    commented ex11

--- week2/ex11/ex11.cc
+++ week2/ex11/ex11.cc
@@ -5,10 +5,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	size_t number=stoul(argv[1]);
+	size_t number=stoul(argv[1]); //Read the input
 
 	for (size_t idx = 1; idx != 11; ++idx)
 	{
+		//Print line of the multiplication table
 		cout << idx << " * " << number << " = " << idx * number << '\n';
 	}
 }
