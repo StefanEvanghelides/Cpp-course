bca6ca4f4aab07898963877ecff182f68ded6f28
Jos van Goor <josvangoor@gmail.com>
2018-09-18 13:54:44 +0200

    Finished??? ex15

--- week2/ex15/ex15.cc
+++ week2/ex15/ex15.cc
@@ -6,7 +6,26 @@ using namespace std;
 int main()
 {
     string line;
-    while(getline(cin, line))       // Read the whitespace head
-        if(line.find_first_not_of(" \t") == string::npos) //if string is not empty
+    while (getline(cin, line))       // Read the whitespace head
+        if (line.find_first_not_of(" \t") != string::npos) //if string is not empty head is cleared.
+        {
+            cout << line;
             break;
+        }
+
+    size_t empty_lines = 1;
+    while (getline(cin, line))
+    {
+        if (line.find_first_not_of(" \t") == string::npos) //if string empty
+        {
+            empty_lines += 1;
+            continue;
+        }
+
+        cout << string(empty_lines, '\n');
+        empty_lines = 1;
+
+        cout << line;
+        
+    }
 }
