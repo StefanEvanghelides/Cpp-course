de976567db462baf212fb74d1a8343c11b21852b
StefanEvanghelides <stefan.evanghelides@gmail.com>
2018-09-18 19:28:12 +0200

    small change in ex14: it now allows for tabs as separators

--- week2/ex14/ex14.cc
+++ week2/ex14/ex14.cc
@@ -6,6 +6,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	string sentence;
+	const string& whiteCharacters = " \t";
 
 	while(!cin.eof())
     {
@@ -17,22 +18,22 @@ int main(int argc, char **argv)
       }
 
       // Trim the strings from starting and ending whitespaces
-      sentence.erase(0, sentence.find_first_not_of(' '));
-      sentence.erase(sentence.find_last_not_of(' ') + 1);
+      sentence.erase(0, sentence.find_first_not_of(whiteCharacters));
+      sentence.erase(sentence.find_last_not_of(whiteCharacters) + 1);
 
-			size_t firstIdx = sentence.find_first_of(' ');
-			size_t lastIdx = sentence.find_last_of(' ');
+			size_t firstIdx = sentence.find_first_of(whiteCharacters);
+			size_t lastIdx = sentence.find_last_of(whiteCharacters);
 
 			string firstWord = sentence.substr(0, firstIdx);
 			string lastWord = sentence.substr(lastIdx + 1);
 
 			sentence.replace(lastIdx + 1, firstIdx, firstWord);
 			if (lastWord.length() > firstWord.length())
-			{
+			{ // If the final word is too big, erase the extra characters
 				sentence.erase(lastIdx + 1 + firstIdx);
 			}
 			sentence.replace(0, firstIdx, lastWord);
-		
+
 			cout << sentence << endl;
     }
 
