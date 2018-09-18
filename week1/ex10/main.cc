0da7b77942b1cfff7060c69959a097bda13ec9c8
Jos van Goor <josvangoor@gmail.com>
2018-09-18 14:33:55 +0200

    added ex13

--- week1/ex10/main.cc
+++ week1/ex10/main.cc
@@ -9,15 +9,14 @@ int main(int argc, char *argv[])
     size_t nibble = stoul(argv[2]);         // nibble to replace
     size_t replacement = stoul(argv[3]) % 16;   // new nibble (= 0 .. 15)
 
-
-    size_t clearMask = 15 << nibble*4; //bitmask used to clear the to be adjusted nibble
+    size_t clearMask = 15 << nibble * 4; //bitmask used to clear the to be adjusted nibble
 
     value = ~value; //Invert all bits in value
     value |= clearMask; //Overwrite the to be adjusted nibble with ones
     value = ~value; //Invert all bits again, the bits in the to be adjusted nibble are now 0
     					// all other bits are unaffected
 
-    value = value | replacement << nibble*4; //Set the to be adjusted nibble to the new value
+    value = value | replacement << nibble * 4; //Set the to be adjusted nibble to the new value
 
     cout << hex << value << '\n';       // show the new value
 }
---
+++ week2/ex13/ex13.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 && string(argv[1]) == "ok")
+    {
+        int line_count = 1;
+        string line;
+        while(getline(cin, line))
+        {
+            cout << line_count++ << " " << line << "\n";
+        }
+    }
+    else
+    {
+        int line_count = 1;
+        string line;
+        while(!cin.eof())
+        {
+            getline(cin, line);
+            cout << line_count++ << " " << line << "\n";
+        }
+    }
+}
