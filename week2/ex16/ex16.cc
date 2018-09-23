fa37cbfa29662b13350f2cadf06e5516ae29bbdd
StefanEvanghelides <stefan.evanghelides@gmail.com>
2018-09-23 19:56:39 +0200

    updated ex16 - localized as much as possible

--- week2/ex16/ex16.cc
+++ week2/ex16/ex16.cc
@@ -3,30 +3,22 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	size_t possibilities = 1 << (argc-1);
 
-	size_t max = 1 << argc; 
-
-	//Iterate upto the largest number possible using 1 bit for every 
-	//command line (CL) argument.
-	//Each of the bits now represents 1 of the CL arguments.
-	//When it is set to 1 in an iteration its corrosponding CL argument
-	//is printed.
-	for (size_t idx = 0; idx != 1 << argc - 1; ++idx) 
+	// Basic algorithm for finding all possible combinations
+	// using bitstrings
+	for (size_t idx = 0; idx != possibilities; ++idx)
 	{	
 		cout << idx + 1 << ": "; //show the line number
 
-		size_t number = idx;
-		for (size_t count = 1; count != argc; ++count)
-		{	
-			if (number & 1)		//If the least significant bit is 1
-				cout << argv[count] << ' '; //Print the corrosponding argument
-
-			number >>= 1;		//Shift the number by 1
-			if (number == 0)	// If there are no more arguments to be printed
-				break;			//Exit the loop
+		for (size_t number = idx, count = 1; count != argc, number != 0; ++count)
+		{
+			if (number & 1)
+				cout << argv[count] << ' ';
+			number >>= 1;
 		}
-		cout << '\n';
+		cout << endl;
 	}
 }
