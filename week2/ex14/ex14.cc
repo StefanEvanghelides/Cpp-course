a187006028de8876199ecace09cc11896f2dddd7
StefanEvanghelides <stefan.evanghelides@gmail.com>
2018-09-23 18:24:09 +0200

    had to change indentation settings in Sublime

--- week2/ex14/ex14.cc
+++ week2/ex14/ex14.cc
@@ -5,17 +5,17 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	string sentence;
-	const string& whiteCharacters = " \t";
+  string sentence;
+  const string& whiteCharacters = " \t";
 
-	while (getline(cin, sentence))
-    {		
-    	int firstIdx = sentence.find_first_of(whiteCharacters);
-		int lastIdx = sentence.find_last_of(whiteCharacters);
-		const string& lastWord = sentence.substr(lastIdx + 1);
-		sentence.replace(0, firstIdx, lastWord);
+  while (getline(cin, sentence))
+  {		
+    int firstIdx = sentence.find_first_of(whiteCharacters);
+    int lastIdx = sentence.find_last_of(whiteCharacters);
+    const string& lastWord = sentence.substr(lastIdx + 1);
+    sentence.replace(0, firstIdx, lastWord);
 
-		cout << sentence << endl;
-    }
+    cout << sentence << endl;
+  }
 
 }
