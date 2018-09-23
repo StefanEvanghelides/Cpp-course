57aaf11ed9e91fe2e948e3aed03625f26bffc6dd
StefanEvanghelides <stefan.evanghelides@gmail.com>
2018-09-23 20:02:08 +0200

    removed LTL from ex 16

--- week2/ex16/ex16.cc
+++ week2/ex16/ex16.cc
@@ -5,20 +5,20 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	size_t possibilities = 1 << (argc-1);
+  size_t possibilities = 1 << (argc-1);
 
-	// Basic algorithm for finding all possible combinations
-	// using bitstrings
-	for (size_t idx = 0; idx != possibilities; ++idx)
-	{	
-		cout << idx + 1 << ": "; //show the line number
+  // Basic algorithm for finding all possible combinations
+  // using bitstrings
+  for (size_t idx = 0; idx != possibilities; ++idx)
+  {	
+    cout << idx + 1 << ": "; //show the line number
 
-		for (size_t number = idx, count = 1; count != argc, number != 0; ++count)
-		{
-			if (number & 1)
-				cout << argv[count] << ' ';
-			number >>= 1;
-		}
-		cout << endl;
-	}
+    for (size_t number = idx, count = 1; count != argc, number != 0; ++count)
+    {
+      if (number & 1)
+        cout << argv[count] << ' ';
+      number >>= 1;
+    }
+    cout << endl;
+  }
 }
