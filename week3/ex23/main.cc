050d0a95cc934a9bcba05a8154d4cdab90d36190
Jos van Goor <josvangoor@gmail.com>
2018-09-24 22:59:44 +0200

    ex23, dont know if i'm using the right implementation for quicksort. also requires a lot of stylewise refactoring

--- week3/ex23/main.cc
+++ week3/ex23/main.cc
@@ -1,9 +1,24 @@
 #include "main.ih"
 
+extern char** environ;
+
 int main(int argc, char **argv)
 try
 {
-    
+    //count number of environment variables.
+    size_t environ_count = 0;
+    while(environ[environ_count] != nullptr)
+        environ_count += 1;
+
+    //copy environment variables to array of strings.
+    string environ_strings[environ_count] = {};
+    for(size_t idx = 0; idx < environ_count; ++idx)
+        environ_strings[idx] = str_to_lower(string{environ[idx]});
+
+    quicksort(environ_strings, 0, environ_count - 1);
+
+    for(size_t idx = 0; idx < environ_count; ++idx)
+        cout << environ_strings[idx] << "\n";
 }
 catch (...)
 {
